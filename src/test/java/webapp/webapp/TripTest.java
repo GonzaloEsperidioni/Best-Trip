e1a9d9b53fc03baad6a1f430ad6ef442af48f261
Gonzalo Esperidioni <gonzalo.esperidioni@despegar.com>
2016-03-10 15:10:20 -0300

    a

--- src/test/java/webapp/webapp/TripTest.java
+++ src/test/java/webapp/webapp/TripTest.java
@@ -6,13 +6,15 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.despegar.jav.domain.Destination;
 import com.despegar.jav.domain.Trip;
 
 public class TripTest {
-	
+	public Destination destination;
 	public Trip trip;
 	 @Before
 	    public void setUp(){
+		 	destination = new Destination("LASTCITYTEST", null);
 	    	trip = new Trip(1000.0);
 	 }
 	 @Test
@@ -21,4 +23,14 @@ public class TripTest {
 		 trip.payAmount(500.0);
 		 assertEquals(trip.getWallet() , 500.0, 0.01);
 	 }
+	 @Test
+	 public void emptyDestinationsTest(){
+		 assertEquals(trip.lastCity(), null);
+	 }
+	 @Test
+	 public void DestinationsTest(){
+		 trip.addDestination(destination);
+		 assertEquals(trip.lastCity(), "LASTCITYTEST");
+	 }
+	 
 }
